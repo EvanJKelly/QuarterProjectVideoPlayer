969acf5991248c32f7793311814d6a0697ac2c97
robert lowry <lowryrob2001@gmail.com>
2019-10-07 16:45:00 -0700

    Created Account Model

---
+++ QuatorProjectVIdeoPlayer/Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuatorProjectVIdeoPlayer.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The theme the website is displayed in
+        /// </summary>
+        public bool DarkMode { get; set; }
+    }
+}
