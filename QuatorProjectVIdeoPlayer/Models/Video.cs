f1b68d885f97331ce525f99d1f229b8f01e7523f
robert lowry <lowryrob2001@gmail.com>
2019-11-13 17:12:06 -0800

    When you click on the thumbnail it will take you to the video viewer page

---
+++ QuatorProjectVIdeoPlayer/Models/Video.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuatorProjectVIdeoPlayer.Models
+{
+    public class Video
+    {
+        public int VideoId { get; set; }
+
+        public string VideoLink { get; set; }
+
+        public string VideoTitle { get; set; }
+
+        public bool Rating { get; set; }
+
+        public string Thumbnail { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
